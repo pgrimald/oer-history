bbe255301e85000499ca29747b9111173c74a022
debshila <debshila@gmail.com>
2018-06-18 02:18:24 -0500

    Adding new script for processing data from 2011-2018

---
+++ supportingScripts/SLCC History 2011-2018.R
@@ -0,0 +1,157 @@
+## Read in and preprocess SLCC dataset
+## DBM, 06/14/17
+
+
+# Setup the necessary packages --------------------------------------------
+rm(list = ls())
+## Get the necessary packages for the analyses
+get.package<- function(package){
+  if (!package %in% installed.packages()){
+    install.packages(package, repos = "http://cran.rstudio.com/")
+  } 
+  invisible(library(package, character.only = TRUE))
+}
+
+## packages required
+packages.needed <- c('tidyverse', 'rapportools', 'prophet','scales','sjPlot', 'optimx', 'Rcpp','lme4', 'kableExtra','lmerTest','readxl', 'captioner','knitr','extrafont', 'lubridate', 'lettercase') 
+suppressMessages(sapply(packages.needed, get.package))
+
+## source ggplot template
+source('~/Documents/oer-history/supportingScripts/ggplot2theme_min.R')
+
+
+# Read in SLCC data -------------------------------------------------------
+SLCC_11_18 <- read_excel('~/Box Sync/SLCC History/oer_data_update_201140-201820.xlsx')
+
+# Read in legend
+SLCC_legend <- read_csv('~/Box Sync/SLCC History/SLCC legend.csv')
+
+# Preprocess dataset for modeling------------------------------------------------------
+### Convert date fields to standardized format
+### Compute Age
+### Mark academic year
+### Mark semesters
+### Round off grades
+### Mark passing grades (>C)
+### Mark before/After OER
+### The Open Ed program was started in the summer of 2014
+
+SLCC_11_18_proc <- SLCC_11_18 %>% 
+  mutate(studentId = factor(PIDM), 
+         instructorId = factor(INSTRUCTOR_PIDM),
+         birthDate = ymd(BIRTH_DATE),
+         termStartDate = ymd(TERM_START_DATE),
+         termEndDate = ymd(TERM_END_DATE)) %>% 
+  mutate(roundedGrade = gsub("\\+|\\-","", FINAL_GRADE),
+         age = as.numeric(round(difftime(termStartDate, birthDate, units = "weeks")/52)),
+         year = year(termStartDate), 
+         year = gsub("20","",year),
+         semester = if_else(month(termStartDate) == 8, "F",
+                            if_else(month(termStartDate) == 5, "Su", "Sp"))) %>% 
+  unite(yrSem, semester, year, sep = " ", remove = FALSE) %>% 
+  left_join(SLCC_legend[ ,c("Final Grade", "Points")], by = c("FINAL_GRADE" = "Final Grade")) %>% 
+  left_join(SLCC_legend[ ,c("yrSem", "academicYear")], by = "yrSem") %>% 
+  mutate(yrSem = factor(yrSem), courseGrade = as.numeric(Points), 
+         termDuration = difftime(termEndDate, termStartDate, units = "days"),
+                                 pass = factor(if_else(roundedGrade <= "C",1,0)),
+                                 completion = factor(if_else(FINAL_GRADE == "I",0,1))) %>% 
+  select(-TERM_START_DATE,-TERM_END_DATE,-BIRTH_DATE, -INSTRUCTOR_PIDM, -PIDM, -Points) 
+
+# Convert names to camel case --------------------------------------------- 
+
+## First conver to lower case
+names(SLCC_11_18_proc)[is_upper(names(SLCC_11_18_proc))] <- tolower(names(SLCC_11_18_proc)[is_upper(names(SLCC_11_18_proc))])
+## Then select names with underscores (_)
+names(SLCC_11_18_proc)[grepl("_", names(SLCC_11_18_proc))] <- lapply(names(SLCC_11_18_proc)[grepl("_", names(SLCC_11_18_proc))], function(nm){
+  nmNo_ <- unlist(strsplit(nm, "_"))
+  ## Select the words other than the first word, and convert the first letter to uppercase
+  followingWords <- paste0(unlist(lapply(strsplit(nmNo_[-1],""), function(w){
+    tmpChar <- unlist(w)
+    tmpChar[1] <- toupper(tmpChar[1])
+    paste0(tmpChar, collapse = "")
+  })), collapse = "")
+  paste0(nmNo_[1], followingWords, collapse = "")
+})
+
+
+
+# Order the year semester combination -------------------------------------
+
+#levels(SLCC_11_18_proc$yrSem)
+## Summer 2011 is added. It needs to be removed
+yrSemOrdered <- paste(rep(c("F", "Sp", "Su"), 7), c(11,rep(12:17, each = 3), 18))
+yrSemOrdered <- yrSemOrdered[-length(yrSemOrdered)]
+
+SLCC_11_18_proc$yrSem <- factor(SLCC_11_18_proc$yrSem, levels = yrSemOrdered)
+
+
+# Duplicate students ------------------------------------------------------
+
+# Some students appear more than once in the dataset
+# They could have taken more than one course, or the same course more than once, or both
+dupeIDs <- SLCC_11_18_proc %>% 
+  filter(duplicated(studentId)) %>% 
+  distinct(studentId) %>% 
+  unlist()
+
+# Add in a column to flag students who appear more than once in the dataset
+SLCC_11_18_proc <- SLCC_11_18_proc %>% 
+  mutate(rep = if_else(studentId %in% dupeIDs,1,0))
+
+# Check the course attributes column by year
+# SLCC_11_18_proc %>% 
+#   group_by(courseAttributes) %>% 
+#   distinct(yrSem) %>% View()
+
+# Locate pilot instructors/sections/courses ------------------------------------------------
+oerPilotSections <- paste0("0", c(12,14,18,22,23, 28, 35,76))
+
+## We are trying to locate the pilot sections for fall 2015
+## We know the course sections for the Sp 16 pilots
+## Get instructor IDs from the sections
+instructorsSpring  <- SLCC_11_18_proc %>% 
+  filter(yrSem == "Sp 16" & courseSection %in% oerPilotSections) %>% 
+  distinct(instructorId) %>% unlist()
+
+## Look at how many courses were taught by each of these instructors
+instructorCourseCountSp16  <- SLCC_11_18_proc %>% 
+  filter(yrSem == "Sp 16" & courseSection %in% oerPilotSections) %>% 
+  group_by(instructorId) %>% 
+  summarise(secTaught = n_distinct(courseSection)) %>%  
+  arrange(instructorId)
+
+
+# Find whether the instructors who taught the pilot sections in Spring 2016, also taught Fall 2015 
+# 5/6 instructors for Sp 16 taught F15
+instructorFall <- SLCC_11_18_proc %>% 
+  filter(yrSem == "F 15" & instructorId %in% instructorsSpring) %>% 
+  distinct(instructorId) %>% 
+  unlist()
+
+## Found how many sections were taught by these instructors
+## One of the Pilot instructors in F15 also taught 4 sections in F15
+## Instructors c(3763, 651843) taught 4 sections each in F15 and are likely to be the pilot instructors
+instructorCourseCountF15 <- SLCC_11_18_proc %>% 
+  filter(yrSem == "F 15" & instructorId %in% instructorsSpring) %>% 
+  group_by(instructorId) %>% 
+  summarise(secTaught = n_distinct(courseSection)) %>%  
+  arrange(instructorId) 
+
+instructorF15 <- instructorCourseCountF15  %>% 
+  filter(secTaught == 4) %>% 
+  select(instructorId) %>% 
+  unlist()
+
+# Add in OER field --------------------------------------------------------
+fullOERSems <- c("F 16", "Sp 17", "Su 17", "F 17", "Sp 18")
+SLCC_11_18_proc <- SLCC_11_18_proc %>% 
+  mutate(oer = if_else(courseSubject == "HIST" & yrSem == "Sp 16" & courseSection %in% oerPilotSections, 1,
+                       if_else(courseSubject == "HIST" & yrSem == "F 15" & instructorId %in% instructorF15, 1, 
+                               if_else(courseSubject == "HIST" & yrSem  %in% fullOERSems, 1, 
+                                       if_else(courseSubject == "POLS" & !is.na(courseAttributes), 1, 0)))))
+
+
+
+# remove original dataset
+rm(SLCC_11_18)
+
--- supportingScripts/SLCCpreProc.R
+++ supportingScripts/SLCCpreProc.R
@@ -13,16 +13,21 @@ get.package<- function(package){
 }
 
 ## packages required
-packages.needed <- c('dplyr', 'rapportools','readr', 'prophet','scales','sjPlot', 'optimx', 'Rcpp','lme4', 'kableExtra','lmerTest','readxl', 'ggplot2', 'tidyr', 'broom', 'captioner','knitr','extrafont', 'lubridate', 'lettercase') 
+packages.needed <- c('tidyverse', 'rapportools', 'prophet','scales','sjPlot', 'optimx', 'Rcpp','lme4', 'kableExtra','lmerTest','readxl', 'captioner','knitr','extrafont', 'lubridate', 'lettercase') 
 suppressMessages(sapply(packages.needed, get.package))
 
 ## source ggplot template
 source('~/Documents/oer-history/supportingScripts/ggplot2theme_min.R')
 
 
-## Read in SLCC data
-SLCCpersistence <- read_csv('~/Box Sync/SLCC History/oer_data.csv')
+# Read in SLCC data -------------------------------------------------------
 
+SLCCpersistence <- read_csv('~/Box Sync/SLCC History/oer_data.csv')
+SLCC_11_18 <- read_csv('Box Sync/SLCC History/oer_data_update_201140-201820.csv')
+teacher_sections <- read_csv("~/Box Sync/SLCC History/Instructor Sections.csv")
+
+SLCCperc11_18 <- SLCCperc11_18 %>% 
+  left_join(teacher_sections)
 # Preprocess dataset for modeling------------------------------------------------------
 ### Convert date fields to standardized format
 ### Compute Age
@@ -61,7 +66,8 @@ SLCCpersistenceProc <- SLCCpersistence %>%
   unite(yrSem, semester, year, sep = " ", remove = FALSE) %>% 
   mutate(yrSem = factor(yrSem), courseGrade = as.numeric(courseGrade),
          termDuration = difftime(termEndDate, termStartDate, units = "days"),
-    oer = factor(if_else(termStartDate >= "2014-05-19" & COURSE_SUBJECT == "HIST", 1, 0)),
+    oer = factor(if_else(termStartDate >= "2015-08-26" & COURSE_SUBJECT == "HIST", 1, 
+                         if_else(termStartDate == "2016-01-11" & COURSE_SECTION  %in% c(paste0("0",c(12,14,18,22,23,28,35,76))) & COURSE_SUBJECT == "HIST",1,0))),
     pass = factor(if_else(roundedGrade <= "C",1,0)),
     dfw = factor(if_else(pass == 1, 0, 1)),
     academicYear = factor(if_else(yrSem %in% c("F 11", "Sp 12", "Su 12"), "11-12",
@@ -72,6 +78,46 @@ SLCCpersistenceProc <- SLCCpersistence %>%
                                                                 if_else(yrSem %in% c("F 16", "Sp 17", "Su 17"), "16-17", "Check")))))))) %>% 
   select(-TERM_START_DATE,-TERM_END_DATE,-BIRTH_DATE, -INSTRUCTOR_PIDM)
   
+## Folding in new data from academic year 2017-2018
+SLCCpersistenceProc17_18 <- SLCCperc17_18 %>% 
+  mutate(studentId = factor(id), 
+         instructorId = factor(INSTRUCTOR_PIDM),
+         birthDate = dmy(BIRTH_DATE),
+         termStartDate = TERM_START_DATE,
+         termEndDate = TERM_END_DATE, 
+         roundedGrade = gsub("\\+|\\-","", FINAL_GRADE),
+         age = as.numeric(round(difftime(termStartDate, birthDate, units = "weeks")/52)),
+         year = year(termStartDate), 
+         year = gsub("20","",year),
+         semester = if_else(month(termStartDate) == 8, "F",
+                            if_else(month(termStartDate) == 5, "Su", "Sp")),
+         courseGrade = if_else(FINAL_GRADE %in% c("E","F"), "0",
+                               if_else(FINAL_GRADE  %in% c("D", "D-"), "1",
+                                       if_else(FINAL_GRADE == "D+", "1.3",
+                                               if_else(FINAL_GRADE == "C-","1.6",
+                                                       if_else(FINAL_GRADE == "C", "2",
+                                                               if_else(FINAL_GRADE == "C+", "2.3",
+                                                                       if_else(FINAL_GRADE == "B-","2.6",
+                                                                               if_else(FINAL_GRADE == "B", "3",
+                                                                                       if_else(FINAL_GRADE == "B+", "3.3",
+                                                                                               if_else(FINAL_GRADE == "A-", "3.6",
+                                                                                                       if_else(FINAL_GRADE == "A","4",
+                                                                                                               if_else(is.na(FINAL_GRADE), "NA", 
+                                                                                                                       if_else(FINAL_GRADE == "A+", "4.3", "-1")))))))))))))) %>% 
+  unite(yrSem, semester, year, sep = " ", remove = FALSE) %>% 
+  mutate(yrSem = factor(yrSem), courseGrade = as.numeric(courseGrade),
+         termDuration = difftime(termEndDate, termStartDate, units = "days"),
+         oer = factor(if_else(termStartDate >= "2015-08-26" & COURSE_SUBJECT == "HIST", 1, 
+                              if_else(termStartDate == "2016-01-11" & COURSE_SECTION  %in% c(paste0("0",c(12,14,18,22,23,28,35,76))) & COURSE_SUBJECT == "HIST",1,0))),
+         pass = factor(if_else(roundedGrade <= "C",1,0)),
+         dfw = factor(if_else(pass == 1, 0, 1)),
+         academicYear = factor(if_else(yrSem %in% c("F 11", "Sp 12", "Su 12"), "11-12",
+                                       if_else(yrSem %in% c("F 12", "Sp 13", "Su 13"), "12-13",
+                                               if_else(yrSem %in% c("F 13", "Sp 14", "Su 14"), "13-14",
+                                                       if_else(yrSem %in% c("F 14", "Sp 15", "Su 15"), "14-15",
+                                                               if_else(yrSem %in% c("F 15", "Sp 16", "Su 16"), "15-16",
+                                                                       if_else(yrSem %in% c("F 16", "Sp 17", "Su 17"), "16-17", "Check")))))))) %>% 
+  select(-TERM_START_DATE,-TERM_END_DATE,-BIRTH_DATE, -INSTRUCTOR_PIDM)
 
 
 ## Convert names to camel case
@@ -115,4 +161,43 @@ yrSemOrdered <- yrSemOrdered[-length(yrSemOrdered)]
 
 SLCCpersistenceProc$yrSem <- factor(SLCCpersistenceProc$yrSem, levels = yrSemOrdered)
 
+# Some students appear more than once in the dataset
+dupeIDs <- SLCCpersistenceProc %>% 
+  filter(duplicated(studentId)) %>% 
+  distinct(studentId) %>% 
+  unlist()
+
+SLCCpersistenceProc <- SLCCpersistenceProc %>% 
+  mutate(rep = if_else(studentId %in% dupeIDs,1,0))
+
+oerPilotSections <- paste0("0", c(12,14,18,22,23, 28, 35, 76))
+
+## We are trying to locate the pilot sections for fall 2015
+## We know the  instructors for fall 2015 pilot sections
+instructorsSpring  <- SLCCpersistenceProc %>% 
+  filter(yrSem == "Sp 16" & courseSection %in% oerPilotSections) %>% 
+  distinct(instructorId) %>% unlist()
+
+instructorCourseCountSp16  <- SLCCpersistenceProc %>% 
+  filter(yrSem == "Sp 16" & courseSection %in% oerPilotSections) %>% 
+  group_by(instructorId) %>% 
+  summarise(secTaught = n_distinct(courseSection)) %>%  
+  arrange(instructorId)
+
+
+# Find whether the instructors who taught the pilot sections in Spring 2016, also taught Fall 2015 
+instructorFall <- SLCCpersistenceProc %>% 
+  filter(yrSem == "F 15" & instructorId %in% instructorsSpring) %>% 
+  distinct(instructorId) %>% 
+  unlist()
+
+instructorCourseCountF15 <- SLCCpersistenceProc %>% 
+  filter(yrSem == "F 15" & instructorId %in% instructorsSpring) %>% 
+  group_by(instructorId) %>% 
+  summarise(secTaught = n_distinct(courseSection)) %>%  
+  arrange(instructorId)
+  
+  
+
+# remove original dataset
 rm(SLCCpersistence)
